e7109d36a9ab07bf9608c082a570fd4d4c7e374c
jruel006 <jruel006@ucr.edu>
2016-06-14 14:38:19 -0700

    tryna get the menu to hide

--- main.js
+++ main.js
@@ -17,7 +17,12 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow( {fullscreen : true});
+  win = new BrowserWindow({
+      fullscreen : true,
+      autoHideMenuBar : true
+  });
+
+  win.setMenu(null)
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/mdl/index.html`);
