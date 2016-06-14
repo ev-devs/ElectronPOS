509315eb02665b3ce3040a6211ea5dd1b8c4b6a7
jruel006 <jruel006@ucr.edu>
2016-06-14 14:31:05 -0700

    added the fullscreen flag

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow( {fullscreen : true});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/mdl/index.html`);
