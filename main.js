1418daac2803170cdb4d44293e18d2f53203c0dd
jruel006 <jruel006@ucr.edu>
2016-06-14 10:09:43 -0700

    About to send to john

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require('electron-prebuilt')
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+app.on('activate', function() {
+    conole.log('we in nigga')
+})
