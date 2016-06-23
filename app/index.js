e19408c49704d16605ed8ac9fa4b230fa859fafc
ColdNightHour <jdixo001@ucr.edu>
2016-06-23 14:23:25 -0700

    remove duplicates works

--- app/index.js
+++ app/index.js
@@ -13,30 +13,42 @@ var fs = require('fs');
 var ejs = require('ejs');
 
 var output = "";
+
 function remove_dup(wifis) {
   /*You may be wondering why? What the hell is this fam? Well it removes the second index of the wifis array because the second index is "" and that is annoying*/
   var i = wifis.indexOf("");
   if(i != -1) {
+    wifis.splice(i, 1);
      wifis.splice(i, 1);
   }
-  /*This new array will hold the wifis without duplicates. */
+  /*This new array will hold the wifis without duplicates.*/
   var connections = [];
-  /*We start at the bottom because by default the connections are sorted from lowets to highest connection quality
+  /*We start at the bottom because by default the connections are sorted from lowest to highest connection quality
   with the worst connection being at the top and best at the bottom*/
   for(var i = wifis.length - 2; i > 1; i--) {
-    console.log(wifis[i].substring(wifis[i].search("\""), wifis[i].length - 1));
-    /*var wifi = {
-      "essid" : wifis[i].substring(wifis.indexOf("ESSID:"), wifis.length - 1),
-      "quality" : "Test"
-    }*/
-    //console.log(wifi.essid);
+    /*Grabs the name of a connection from an index in the wifi array, excluidng the double quotes around the name*/
+    var essid = wifis[i].substring(wifis[i].search("\"") + 1, wifis[i].lastIndexOf("\""));
+    /*Grabs whether or not the connection needs a passkey, so either on or off*/
+    var psk = wifis[i].substring(wifis[i].search(":o") + 1, wifis[i].search("ES") - 1);
+    /*Combines them into one string*/
+    var combined = essid + "|" + psk;
+    /*If that string is not found within the connections array then put it in. If it is then exclude it. */
+    if(connections.indexOf(combined) == -1) {
+      connections.push(combined);
+    }
   }
+  return connections
+}
+
+fuction JSONify(connections) {
+  for(var i = 0; i < connections.length - 1; i++)
 }
 /*This calls our function*/
 function list_connections(){
+    /*Grabs the output ofthe script and makes it into an array*/
     output = execSync('sudo ' + __dirname + '/../pw/wifi_script.sh').toString('utf-8').split('\n');
-    remove_dup(output);
-    return output.toString('utf-8').split('\n');
+    /*Removes any duplicate essids*/
+    output = remove_dup(output);
 }
 
 
