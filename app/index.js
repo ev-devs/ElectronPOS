9da33cb453ba9a031c010e3f756297fdd0fd5ed3
jruel006 <jruel006@ucr.edu>
2016-06-27 16:45:37 -0700

    testing out the modal trigger in the index.js

--- app/index.js
+++ app/index.js
@@ -38,5 +38,17 @@ $(document).on('click', '#accept', function() {
   /**$("#cur_connection").text() = cur;**/
 });
 
+
+/*Used to trigger the modal located in _index.ejs*/
+$('.modal-trigger').leanModal({
+     dismissible: true, // Modal can be dismissed by clicking outside of the modal
+     opacity: .5, // Opacity of modal background
+     in_duration: 300, // Transition in duration
+     out_duration: 200, // Transition out duration
+   }
+ );
+/*
+
+
 /*WE LAUNCH OUR SERVER TO START SESSIONS*/
 require('./lib/server.js')
