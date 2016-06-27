e91e8d1f215e248faab1b4275527d0d8876eb7a2
jruel006 <jruel006@ucr.edu>
2016-06-27 16:24:25 -0700

    Changing stuffs for john

--- app/lib/internet.js
+++ app/lib/internet.js
@@ -39,7 +39,7 @@ var internet = {
           connections[i] = wifi;
         }
         /*Runs the wifi_cur.sh script to get the output and store it as a string*/
-        var cur = execSync('sudo '+ __dirname + '/../../pw/wifi_cur.sh').toString();
+        var cur = execSync('sudo ' + __dirname + '/../../pw/wifi_cur.sh').toString();
         /*pushes it as the last element for easy access*/
         connections.push(cur);
         return connections;
