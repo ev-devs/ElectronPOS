b173aae8bb36a9afe252bb945854640023bab8a9
jruel006 <jruel006@ucr.edu>
2016-07-19 17:41:36 -0700

    continuing work on sessions

--- app/views/03-beginsession/backend.js
+++ app/views/03-beginsession/backend.js
@@ -33,4 +33,45 @@ iboSession.find({}, function(err, sessions){
     console.log(sessions)
 });
 
-$('')
+$('.begin-session').click(function(event){
+    // event is a click event just FYI
+    if (noErrors()){
+        
+    }
+
+});
+
+/*returns true if no errors. False if error is detected*/
+function noErrors(){
+
+    var return_value = true;
+
+    if($('#first_name').val() == ""){
+        Materialize.toast('Please Input A First Name', 4000)
+        return_value = false
+    }
+    if ($('#last_name').val() == ""){
+        Materialize.toast('Please Input A Last Name', 4000)
+        return_value = false
+    }
+    if ($('#ibo_number').val() == ""){
+        Materialize.toast('Please Input An IBO Number', 4000)
+        return_value = false
+    }
+    return return_value
+}
+
+function iboExists() {
+    iboSession.findOne({
+        firstname   : $('#first_name').val(),
+        lastname    : $('#last_name').val(),
+        ibonumber   : $('#ibo_number').val()
+    }. function(err, ibo){
+        if (err){
+            return console.log(err)
+        }
+        else {
+
+        }
+    })
+}
