09078ca20fce92ad8017b370ea726aeb6a59f00c
jruel006 <jruel006@ucr.edu>
2016-07-31 16:03:10 -0700

    fixing this thing

--- app/views/04-pos/compiled.js
+++ app/views/04-pos/compiled.js
@@ -147,7 +147,7 @@ $(document).on("click", ".platinum", function() {
 	scan_flag = 1;
   current_platinum = $(this).attr("id");
   $("#" + current_platinum).addClass("green lighten-3");
-	$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '../partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
+	$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/../partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
 	//$("#current-platinum").attr("placeholder", current_platinum.replace(/1/g, " ").replace(/2/g, ","));
 });
 
@@ -155,7 +155,7 @@ $("#platinum").click(function() {
 	if(current_platinum != "NONE" && confirm_flag == 1) {
 		current_platinum = "NONE";
 		confirm_flag = 0;
-		$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '../partials/select_platinums.html', 'utf-8') , {"A" : 0}));
+		$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/../partials/select_platinums.html', 'utf-8') , {"A" : 0}));
 	}
 })
 
@@ -680,102 +680,6 @@ $(document).on("click",  "#confirm_item_selection", function() {
 
 $(document).on("click",  "#cancel_item_selection", function() {
 	$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
-});
-
-/*
-
-tl;dr This contains all the logic for the pos.html view
-
-file name   : pos.js
-full path   : app/views/pos/pos.js
-purpose     : manipulate the POS GUI when scanning items
-              as well as communication with the EMV reader (to charge client)
-present in  : pos.html
-
-*/
-var request = require('request');
-//TO be removed once connected to views
-var ejs = require('ejs');
-var fs = require('fs');
-var accounting = require('accounting-js');
-var _ = require("underscore");
-// Global variables
-var inventory = [];
-var URL = process.env.EQ_URL.toString();
-var leaders_list = [];
-var list_names = [];
-
-/*Leaders*/
-//Lists leaders in alphabetical order
-// appends html element to display all the names
-// if search is changed, takes search input and reduces html elements to display elements with
-// the searched word.
-// if searched word is not found, displays no results notification
-// if search is empty,
-
-function alphabetize(list){
-	var name = "";
-	for(var i = 0; i < list.length; i++){
-		name = list[i].lastname.toString()  + ", " + list[i].firstname.toString();
-		leaders_list.push(name);
-	}
-	leaders_list.sort();
-}
-
-
-//take user input .change(function(){})   DONE
-//convert to string .val()     DONE
-//convert string into regex    var re = new RegExp("a|b", "i");
-//search for regex in each element of the array array[i].search(regex)
-//if regex is found, NOT -1, then get the index
-// change to list to show in the browser
-function selectPlatinum(list, searched){
-	var user_input = "";
-	var name = "";
-	$("#enter-platinum").change(function(){
-			user_input = $("#enter-platinum").val();
-			if(user_input != ""){
-				searched = [];
-				var re = new RegExp(user_input.toString(), "i");
-				for(var i = 0; i < list.length; i++){
-					if(list[i].search(re) != -1){
-						searched.push(list[i]);
-					}
-				}
-				console.log(searched);
-			}
-		});
-
-}
-function display_list(list){
-	var name = "";
-	for(var i = 0; i < list.length; i++){
-		 name = "<a href=\"#!\" class=\"collection-item\">" + list[i].toString() + "</a>";
-		 $("#platinums-list").append(name);
-	}
-}
-
-request({
-		method: 'POST',
-		uri: URL + '/evleaders',
-		form: {
-			token: process.env.EQ_TOKEN.toString()
-		}
-		}, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var leaders = [];  // container for the leaders object
-					leaders = JSON.parse(body).evleaders; // gets list of leaders and puts it in container called leaders
-					alphabetize(leaders); // gets leaders in alphabetic order places the result in leaders_list
-					display_list(leaders_list);
-					selectPlatinum(leaders_list, list_names)
-					$('#platinums-list').show()
-					$('.loading').hide()
-
-				} else if (error) {
-					console.log(error);
-				} else {
-					console.log(body);
-				}
 });
 
 /*********************************************NOTE: BEGIN SCAN CODE*********************************************/
