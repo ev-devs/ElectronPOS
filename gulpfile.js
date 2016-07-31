7e7bb20d3c1e79b1c32e4ad33b263a46514b2e10
jruel006 <jruel006@ucr.edu>
2016-07-31 16:01:06 -0700

    Please fucking work

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,7 @@ var watch 	= require('gulp-watch');
 
 gulp.task('default', function() {
 	gutil.log('This is an empty task')
-	return
-
-	return gulp.src('./app/views/04-pos/lib/*.js')
-	.pipe(concat('compiled.js'))
-	.pipe(gulp.dest('./app/views/04-pos/'));
 });
-
-/*
-gulp.task('stream', function(){
-	return watch('./app/views/04-pos/backend/*.js', { ignoreInitial: false })
-	.pipe(gulp.dest('./app/views/04-pos/backend/'));
-});
-*/
 
 gulp.task('compile', function(){
 	gulp.src('./app/views/04-pos/lib/*.js')
