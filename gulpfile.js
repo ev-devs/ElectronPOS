d879106717a42632f5544951aa72d61eb988cd22
jruel006 <jruel006@ucr.edu>
2016-07-31 15:11:49 -0700

    pushing againg

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+var gulp 	= require('gulp');
+var gutil 	= require('gulp-util');
+var concat 	= require('gulp-concat');
+var watch 	= require('gulp-watch');
 
 gulp.task('default', function() {
+	gutil.log('Started default gulp task')
 	return gulp.src('./app/views/04-pos/backend/*.js')
 	.pipe(concat('compiled.js'))
 	.pipe(gulp.dest('./app/views/04-pos/'));
 });
+
+gulp.task('stream', function(){
+	return watch('./app/views/04-pos/backend/*.js', { ignoreInitial: false })
+	 .pipe(gulp.dest('./app/views/04-pos/'));
+})
