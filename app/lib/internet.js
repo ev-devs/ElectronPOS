421f5fc52b5635553f97d53f00e8744b409c5c2a
ColdNightHour <jdixo001@ucr.edu>
2016-08-01 15:25:34 -0700

    Added some stuff for wifi

--- app/lib/internet.js
+++ app/lib/internet.js
@@ -66,6 +66,8 @@ var psk = "";
 /*Upon accepting a Wi-Fi connection this funtion will run the script which handels connection. If the password is wrong then the connection will
 not happen*/
 $(document).on('click', '#accept', function() {
+  execSync("sudo " + __dirname + "/../../dixonconnect/wifi_rem.sh ");
+  psk = $("#keyboard").val();
   $(".modal-content-pw").empty();
   $(".modal-content-pw").append(
     "<div class=\"modal-content\"> \
@@ -76,8 +78,6 @@ $(document).on('click', '#accept', function() {
         <div class=\"modal-footer\"> \
           <a href=\"#!\" class=\" modal-action modal-close waves-effect waves-green btn-flat\">Ok</a> \
         </div>")
-  execSync("sudo " + __dirname + "/../../dixonconnect/wifi_rem.sh ");
-  psk = $("#keyboard").val();
    if(psk.search("#") != -1) {
     console.log("PRESENT");
     psk = psk.replace(/#/g, "\\#");
