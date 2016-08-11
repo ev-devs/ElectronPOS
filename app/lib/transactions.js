3eb5cc39a626b1a30a01552862a4fc7efee16847
jruel006 <jruel006@ucr.edu>
2016-08-11 15:02:18 -0700

    almost done with creating a new transaction schema

--- app/lib/transactions.js
+++ app/lib/transactions.js
@@ -1,19 +1,44 @@
 /*second we create a schema of how our data will be modeled*/
+var Cash = new mongoose.Schema({
+    guid        : { type : String, required : true }
+    tendered    : { type : Number, required : true },
+    change      : { type : Number, required : true },
+})
+
+var Card = new mongoose.Schema({
+    amount      : { type : Number, required : true },
+    authCode    : { type : String, required : true },
+    transId     : { type : String, required : true },
+    message     : { type : String, required : true },
+    cardType    : { type : String, required : true }
+})
+
+var Item =  new mongoose.Schema({
+
+})
+
+
+
 var transactionSchema = new mongoose.Schema({
-    _type          : { type : String },
-    price          : { type : String },
-    transId        : { type : String, unique : true },
-    message        : { type : String },
-    authCode       : { type : String }
+
+    items       : [ Item ],
+    subtotal    : { type : Number, required : true },
+    tax         : { type : Number, required : true },
+    total       : { type : Number, required : true },
+    cashes      : [ Cash ],
+    cards       : [ Card ]
 
 });
 
+
 /*now we create an actual model we can use to communicatte with our javascript*/
+
 var Transaction = TransactionConnection.model('Transaction', transactionSchema);
+
 /*
-    PlatinumConnection is defined within the backend.js file
+    transaction connection is defined within the backend.js file
     and it is its own seperate database with a dedicated connection
-    to ONLY platinums
+    to ONLY transactions
 */
 
 module.exports = Transaction
