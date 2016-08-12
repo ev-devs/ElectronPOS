edcdca40ce83ff68036bbf09e551f91fe0810879
ColdNightHour <jdixo001@ucr.edu>
2016-08-12 13:05:12 -0700

    TEsting this weirness out

--- app/views/04-pos/compiled.js
+++ app/views/04-pos/compiled.js
@@ -592,6 +592,30 @@ $("#n_delete").click(function() {
   refocus();
 });
 
+const ipc = require('electron').ipcRenderer
+
+$('#end-session').click(function(event){
+
+    if ( transactionIsInProgress() ){
+        // do error handling
+    }
+    else {
+        ipc.send('ibo-session-end', 'ending session now')
+    }
+
+})
+
+// returns true if transaction is in progress
+function transactionIsInProgress(){
+    // chcek to see if the plane is completely empty
+}
+
+ipc.on('ibo-session-end-reply', function (event, arg) {
+  const message = `Asynchronous message reply from main process: ${arg}`
+  console.log(message)
+
+})
+
 document.addEventListener('refocus', function(e) {
   $("#barcode").focus();
 })
@@ -627,29 +651,84 @@ function fade_out() {
 
  $(".button-collapse").sideNav();
 
-const ipc = require('electron').ipcRenderer
-
-$('#end-session').click(function(event){
-
-    if ( transactionIsInProgress() ){
-        // do error handling
+/*********************************************NOTE: BEGIN UPDATE  PRICE CODE*********************************************/
+function update_price(operation, quantity, placement, confirmed) {
+    if(!confirmed) {
+        /*Update the global quantities of subtotal, tax, and total*/
+        if(operation == '+')
+            subtotal+=((item_list[placement].price * quantity));
+        else if(operation == '-')
+            subtotal-=((item_list[placement].price * quantity));
+        else if(operation == '~')
+            subtotal-=quantity;
+        $("#subtotal").text("$" + accounting.formatNumber(subtotal, 2, ",").toString());
+        tax = subtotal * .075;
+        $("#tax").text("$" + accounting.formatNumber(tax, 2, ",").toString());
+        total = subtotal + tax;
+        $("#total").text("$" + accounting.formatNumber(total, 2, ",").toString());
     }
-    else {
-        ipc.send('ibo-session-end', 'ending session now')
+    else if(confirmed) {
+        total-=quantity;
+        $("#total").text("$" + accounting.formatNumber(total, 2, ",").toString());
     }
-
-})
-
-// returns true if transaction is in progress
-function transactionIsInProgress(){
-    // chcek to see if the plane is completely empty
 }
 
-ipc.on('ibo-session-end-reply', function (event, arg) {
-  const message = `Asynchronous message reply from main process: ${arg}`
-  console.log(message)
 
-})
+/*********************************************NOTE: BEGIN VOID ORDER CODE*********************************************/
+/*A function that voids an order. Used to cancel orders and void orders aftercash or card has been paid*/
+function void_order(full_void) {
+    confirm_flag = 0;
+    cancel_flag = 0;
+    /*Cash flag is set to 0 to denote the end of a cash transaction*/
+    cash_flag = 0;
+    /**/
+    card_flag = 0;
+    scan_flag = 0;
+    ticket_flag = 0;
+    swipe_flag = 0;
+    current_ticket = [-1, -1, "CODE"];
+    if(full_void == 1) {
+    item_list.splice(0, item_list.length);/*Empties the item list*/
+        /*Empties the left side*/
+    $("#sale_list tbody").empty();
+        /*Empties the subtotal and total*/
+        update_price('~', subtotal, 0, 0);
+    $("#cancel").removeAttr("style");
+    $("#confirm").removeAttr("style");
+    /*Sets the confirm flag back to one to denote that a normal completion can happen*/
+    current_platinum = "NONE";
+        previous_page = "1";
+        current_page = "2";
+    setTimeout(function() {
+      $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/select_platinums.html', 'utf-8') , {"A" : 0}));
+    }, 1500);
+    }
+}
+
+function colorfy() {
+    /*Sets the cancel and confirm buttons to red and green respectively*/
+    $("#cancel").css("background-color", "red");
+    $("#confirm").css("background-color", "green");
+}
+
+/*********************************************BEGIN ERROR MODAL CODE*********************************************/
+function error_platinum() {
+    $('#modal4').openModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+    });
+}
+
+function error_in_used() {
+    $('#modal5').openModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+    });
+}
 
 /**********************************************NOTE: BEGIN SEARCH INVENTORY CODE*********************************************/
 /*var i_i = -1;
@@ -979,83 +1058,4 @@ function add_item(item_list_index, inventory_list_index, quantity, manual) {
 	cancel_flag = 1;
 	/*Update the global quantities of subtotal, tax, and total*/
 	update_price('+', quantity, item_list_index, 0);
-}
-
-/*********************************************NOTE: BEGIN UPDATE  PRICE CODE*********************************************/
-function update_price(operation, quantity, placement, confirmed) {
-    if(!confirmed) {
-        /*Update the global quantities of subtotal, tax, and total*/
-        if(operation == '+')
-            subtotal+=((item_list[placement].price * quantity));
-        else if(operation == '-')
-            subtotal-=((item_list[placement].price * quantity));
-        else if(operation == '~')
-            subtotal-=quantity;
-        $("#subtotal").text("$" + accounting.formatNumber(subtotal, 2, ",").toString());
-        tax = subtotal * .075;
-        $("#tax").text("$" + accounting.formatNumber(tax, 2, ",").toString());
-        total = subtotal + tax;
-        $("#total").text("$" + accounting.formatNumber(total, 2, ",").toString());
-    }
-    else if(confirmed) {
-        total-=quantity;
-        $("#total").text("$" + accounting.formatNumber(total, 2, ",").toString());
-    }
-}
-
-
-/*********************************************NOTE: BEGIN VOID ORDER CODE*********************************************/
-/*A function that voids an order. Used to cancel orders and void orders aftercash or card has been paid*/
-function void_order(full_void) {
-    confirm_flag = 0;
-    cancel_flag = 0;
-    /*Cash flag is set to 0 to denote the end of a cash transaction*/
-    cash_flag = 0;
-    /**/
-    card_flag = 0;
-    scan_flag = 0;
-    ticket_flag = 0;
-    swipe_flag = 0;
-    current_ticket = [-1, -1, "CODE"];
-    if(full_void == 1) {
-    item_list.splice(0, item_list.length);/*Empties the item list*/
-        /*Empties the left side*/
-    $("#sale_list tbody").empty();
-        /*Empties the subtotal and total*/
-        update_price('~', subtotal, 0, 0);
-    $("#cancel").removeAttr("style");
-    $("#confirm").removeAttr("style");
-    /*Sets the confirm flag back to one to denote that a normal completion can happen*/
-    current_platinum = "NONE";
-        previous_page = "1";
-        current_page = "2";
-    setTimeout(function() {
-      $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/select_platinums.html', 'utf-8') , {"A" : 0}));
-    }, 1500);
-    }
-}
-
-function colorfy() {
-    /*Sets the cancel and confirm buttons to red and green respectively*/
-    $("#cancel").css("background-color", "red");
-    $("#confirm").css("background-color", "green");
-}
-
-/*********************************************BEGIN ERROR MODAL CODE*********************************************/
-function error_platinum() {
-    $('#modal4').openModal({
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        opacity: .5, // Opacity of modal background
-        in_duration: 300, // Transition in duration
-        out_duration: 200, // Transition out duration
-    });
-}
-
-function error_in_used() {
-    $('#modal5').openModal({
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        opacity: .5, // Opacity of modal background
-        in_duration: 300, // Transition in duration
-        out_duration: 200, // Transition out duration
-    });
 }
