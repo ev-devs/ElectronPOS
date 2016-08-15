57655b6e5ec506e0713cea4714a9f5f63a83a0bc
jruel006 <jruel006@ucr.edu>
2016-08-15 15:33:18 -0700

    deleteing the jboard modal

--- app/views/04-pos/lib/functions.js
+++ app/views/04-pos/lib/functions.js
@@ -48,6 +48,7 @@ function void_order(full_void) {
       cur_transaction = {};
       setTimeout(function() {
           $('#enter-platinum').remove()
+          $('#enter-platinum-modal').remove()
           $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/select_platinums.html', 'utf-8') , {"A" : 0}));
       }, 1500);
     }
