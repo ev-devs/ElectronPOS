38a009d9687c72eedf338f5d8d97253dc73277c4
ColdNightHour <jdixo001@ucr.edu>
2016-08-31 17:07:25 -0700

    Adding changes

--- app/views/04-pos/lib/scan.js
+++ app/views/04-pos/lib/scan.js
@@ -1,70 +1,16 @@
 /***********************SCAN.JS***********************/
-/*When the #scan_sim button is click carry out the following callback*/
-/*$(document).on("input", "#barcode", function()  {
 
-  $('#modal8').openModal({
-    dismissible: false, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
-    in_duration: 300, // Transition in duration
-    out_duration: 200, // Transition out duration
-  });
-});
-*/
 $("#barcode").change(function() {
   /*Grab the barcode from the text area about*/
   var barcode = $("#barcode").val();
   /*Pass into  this function, which is defined below. See the function to know what it does.*/
+  /*BRANCH which handles ticket transactions*/
 	var k = -1;
 	if(barcode[0] == '2' && barcode.length != 1 && current_platinum != "NONE") {
 		k = verify_ticket(barcode);
 	}
-	var ticket;
-	/*BRANCH which handles ticket transactions*/
 	if(k != -1 && current_platinum != "NONE" && ticket_table.get(barcode) == undefined) {
     handle_tickets(barcode);
-		/*if(ticket_flag == 0) {
-        console.log("A");
-				ticket_flag = 1;
-				confirm_flag = 0;
-				cancel_flag = 0;
-        previous_ticket = barcode;
-				$('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/tickets.html', 'utf-8') , {}));
-        refocus();
-		}
-		/*Add <= 50 functionality here
-		else if(ticket_flag == 1) {
-      console.log("B");
-      var itm_qnt = Number(barcode.substring(6, barcode.length - 1)) - Number(current_ticket[2]) + 1;
-      if(itm_qnt > 50) {
-        $('#modal7').openModal({
-          dismissible: false, // Modal can be dismissed by clicking outside of the modal
-          opacity: .5, // Opacity of modal background
-          in_duration: 300, // Transition in duration
-          out_duration: 200, // Transition out duration
-        });
-      }
-			else if(current_ticket[1] == -1) {
-				//ticket = Object.assign({}, inventory[current_ticket[0]])
-        ticket = inventory[current_ticket[0]];
-				ticket.cust_quantity = (itm_qnt);
-        /*ADD TO HASHTABLE
-				item_list.push(ticket);
-				current_ticket[1] = item_list.length - 1;
-				add_item(current_ticket[1], current_ticket[0], ticket.cust_quantity, 1)
-        add_to_table(previous_ticket, ticket.cust_quantity);
-        $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
-			}
-			else if(current_ticket[1] != -1) {
-				item_list[current_ticket[1]].cust_quantity+=(itm_qnt);
-				add_item(current_ticket[1], current_ticket[0], item_list[current_ticket[1]].cust_quantity, 0)
-        add_to_table(previous_ticket, itm_qnt);
-        $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
-			}
-			ticket_flag = 0;
-			confirm_flag = 1;
-			cancel_flag = 1;
-      refocus();
-		}*/
 	}
 	else if(ticket_table.get(barcode) != undefined) {
 		ticket_flag = 0;
@@ -77,7 +23,11 @@ $("#barcode").change(function() {
 
 
 	/*Handles transactions other than tickets*/
+  else if(current_page == "prev_trans.html") {
+    console.log("SEXY");
+  }
 	else if(k == -1 && current_platinum != "NONE"){
+    console.log("X")
 	  var i;
 	  var places = [];
 	  if(current_platinum != "NONE" && scan_flag == 1)
@@ -95,92 +45,6 @@ $("#barcode").change(function() {
 		error_platinum();
 	}
   $("#barcode").val("");
-});
-
-$("#scan_sim").click(function(){
-  /*Grab the barcode from the text area about*/
-  var barcode = $("#barcode").val();
-  /*Pass into  this function, which is defined below. See the function to know what it does.*/
-	var k = -1;
-	if(barcode[0] == '2' && barcode.length != 1 && current_platinum != "NONE") {
-		k = verify_ticket(barcode);
-	}
-	//var ticket;
-	/*BRANCH which handles ticket transactions*/
-	if(k != -1 && current_platinum != "NONE" && ticket_table.get(barcode) == undefined) {
-    handle_tickets();
-    if(ticket_flag == 0) {
-        console.log("A");
-        ticket_flag = 1;
-        confirm_flag = 0;
-        cancel_flag = 0;
-        previous_ticket = barcode;
-        $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/tickets.html', 'utf-8') , {}));
-        refocus();
-    }
-    /*Add <= 50 functionality here*/
-    else if(ticket_flag == 1) {
-      console.log("B");
-      var itm_qnt = Number(barcode.substring(6, barcode.length - 1)) - Number(current_ticket[2]) + 1;
-      if(itm_qnt > 50) {
-        $('#modal7').openModal({
-          dismissible: false, // Modal can be dismissed by clicking outside of the modal
-          opacity: .5, // Opacity of modal background
-          in_duration: 300, // Transition in duration
-          out_duration: 200, // Transition out duration
-        });
-      }
-      else if(current_ticket[1] == -1) {
-        //ticket = Object.assign({}, inventory[current_ticket[0]])
-        ticket = inventory[current_ticket[0]];
-        ticket.cust_quantity = (itm_qnt);
-        /*ADD TO HASHTABLE*/
-        item_list.push(ticket);
-        current_ticket[1] = item_list.length - 1;
-        add_item(current_ticket[1], current_ticket[0], ticket.cust_quantity, 1)
-        add_to_table(previous_ticket, ticket.cust_quantity);
-        $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
-      }
-      else if(current_ticket[1] != -1) {
-        item_list[current_ticket[1]].cust_quantity+=(itm_qnt);
-        add_item(current_ticket[1], current_ticket[0], item_list[current_ticket[1]].cust_quantity, 0)
-        add_to_table(previous_ticket, itm_qnt);
-        $('#right-middle').html(ejs.render(fs.readFileSync( __dirname + '/partials/handle_order.html', 'utf-8') , {"platinum" : current_platinum.replace(/1/g, " ").replace(/2/g, ",")}));
-      }
-      ticket_flag = 0;
-      confirm_flag = 1;
-      cancel_flag = 1;
-      refocus();
-    }
-	}
-	else if(ticket_table.get(barcode) != undefined) {
-		ticket_flag = 0;
-		error_in_used();
-	}
-	else if(k == -1 && ticket_flag == 1 && current_platinum != "NONE") {
-		$("#errors").text("Error! Please scan a ticket!");
-	}
-	/*END TICKET HANDLING CODE*/
-
-
-	/*Handles transactions other than tickets*/
-	else if(k == -1 && current_platinum != "NONE"){
-	  var i;
-	  var places = [];
-	  if(current_platinum != "NONE" && scan_flag == 1)
-	    places = determine_item_status(item_list, inventory, barcode);
-	  i = places[1]; //item_list_index
-		j = places[0]; //inventory_list_index
-	  /*If the item in the list has a quantity of one then this means it is not present on the gui and must be put into the gui
-	  with the code below.*/
-	  if(i != -1 && current_platinum != "NONE" && scan_flag == 1) {
-			add_item(i, j, 1, 0);
-		}
-	  $("#barcode").focus();
-	}
-	else {
-		error_platinum();
-	}
 });
 
 /*Finds the specified item in the list, returns -1 if not found.*/
